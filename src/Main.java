66387de11cdeb6754d7213a9056c9f6fcbed5799
szczur4 <rybaglut1@gmail.com>
2024-10-10 20:36:43 +0200

    optimized

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Main{
-	static Vector<Long>v=new Vector<>();
+	static ArrayList<Integer>v=new ArrayList<>();
 	static boolean b;
-	static short z;
-	static File file=new File("C:\\Users\\<USER>\\Desktop\\primes.txt");
-	static FileWriter writer;
+	static int a=2,z;
+	static File file=new File("C:\\Users\\szczur4\\Desktop\\primes.txt");
+	static BufferedWriter writer;
 	public static void main(String[] args)throws Exception{
 		file.createNewFile();
-		writer=new FileWriter(file);
-		v.add(2L);
-		for(long i=3;i<Integer.MAX_VALUE;i+=2){
+		writer=new BufferedWriter(new FileWriter(file));
+		v.add(2);
+		writer.write(2+" ");
+		for(int i=3;i<Integer.MAX_VALUE;i+=2){
 			b=true;
-			for(int j=0;j<v.size()&&v.get(j)<Math.sqrt(i);j++)if(i%v.get(j)==0){
+			for(int j=0;j<v.size()&&v.get(j)<=Math.sqrt(i);j++)if(i%v.get(j)==0){
 				b=false;
 				break;
 			}
 			if(b){
 				z++;
-				if(z==10000){
+				if(z==1000000){
+					System.out.println(i);
 					writer.flush();
 					z=0;
 				}
-				writer.write(i+" ");
+				writer.write(i-a+" ");
+				a=i;
 				v.add(i);
 			}
 		}
